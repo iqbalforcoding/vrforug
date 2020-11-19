14957e7deea89fc0d762070cd0fd9b92cf3f718c
iqbalforcoding <boxbeat97@gmail.com>
2020-11-19 16:50:18 +0700

    third

---
+++ Assets/Exit.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Exit : MonoBehaviour
+{
+
+    public float gazeTime = 2f;
+    private float timer;
+    private bool gazedAt;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= gazeTime)
+            {
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+                timer = 0f;
+            }
+        }
+    }
+
+
+    public void PointerEnter()
+    {
+        if (gazedAt=true){
+            Application.Quit();
+            UnityEngine.Debug.LogError("Keluar");
+        }
+    }
+
+}
---
+++ Assets/ShowHideInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideInfo : MonoBehaviour {
+
+    public GameObject InfoObject;
+    private bool Show = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowAndHideInfo()
+    {
+        if (!Show)
+        {
+            InfoObject.SetActive(true);
+            Show = true;
+        }
+
+        else
+        {
+            InfoObject.SetActive(false);
+            Show = false;
+        }
+    }
+}
